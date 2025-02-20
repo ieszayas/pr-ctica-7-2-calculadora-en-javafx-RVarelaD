f1e1dab4a4f890c3597619acd8d2b1861349f9ca
RVarelaD <rodvado@gmail.com>
2025-02-20 16:16:04 +0100

    Revert "Commit Parte_1"
    This reverts commit 08a334241134e9ba71c5a321804f57648c5c098a.

--- Parte1/Parte_1_PR7_2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.parte_3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens Main to javafx.fxml, javafx.graphics;  // <-- Asegura que se abre a javafx.graphics
-    exports Main;  // <-- Exporta Main para que sea accesible
-}
